b565c9b284291edd5bcbc1709bd02499cb8314ff
Jacob Harvey <jacobharv00@gmail.com>
2021-02-02 02:58:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay([musicians], [instruments]) {
   var taco = []
-  for( i = 0; i > 0; i++)
+  for( i = 0; i < 0; i++)
   var newArray = [`'John lennon plays gutiar'`]
     return newArray
   }
