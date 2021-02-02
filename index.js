503a7c9639ff97aa7d46bfcae547ab9e1e96f17a
Jacob Harvey <jacobharv00@gmail.com>
2021-02-02 02:57:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay([musicians], [instruments]) {
+  var taco = []
+  for( i = 0; i > 0; i++)
+  var newArray = [`'John lennon plays gutiar'`]
+    return newArray
+  }
+}
