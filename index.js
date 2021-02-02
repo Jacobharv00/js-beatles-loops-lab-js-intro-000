daf20c2681269c0e45d92004ea02beb0c341cc4e
Jacob Harvey <jacobharv00@gmail.com>
2021-02-02 03:13:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,14 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var arr = []
   for ( let i = 0; i < musicians.length; i++) {
-    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+    arr.push(`${musicians![i]} plays ${instruments![i]}`)
   }
   return arr
 }
-
-
-
-
-
-
-//arr.push(`${musicians[i]} plays ${instruments[i]}`)
